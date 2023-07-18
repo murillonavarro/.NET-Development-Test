d4a202a80abc98263e2f4afae7c428fa8a6d7516
Murillo Navarro <murillobilchesnavarro@gmail.com>
2023-07-18 18:27:32 -0300

    Adicionando mais uma opção de log.

--- Development-Test/Development-Test/Program.cs
+++ Development-Test/Development-Test/Program.cs
@@ -6,6 +6,7 @@ public class Program
     public static async Task Main(string[] args)
     {
         // Exemplo de uso do logger
+        await Logger.Instance.LogAsync(LogLevel.Debug, "Esta é uma mensagem de log no nível Debug.");
         await Logger.Instance.LogAsync(LogLevel.Info, "Essa é uma mensagem de informação.");
         await Logger.Instance.LogAsync(LogLevel.Warning, "Essa é uma mensagem de aviso.");
         await Logger.Instance.LogAsync(LogLevel.Error, "Essa é uma mensagem de erro.");
