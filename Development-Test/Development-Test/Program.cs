f4f4bfbf86d55d9818cae3618a1d7ed15859d888
Murillo Navarro <murillobilchesnavarro@gmail.com>
2023-07-18 14:47:49 -0300

    Adicionando o código de teste

--- Development-Test/Development-Test/Program.cs
+++ Development-Test/Development-Test/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Development_Test.Enums;
+using Development_Test.Models;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // Exemplo de uso do logger
+        await Logger.Instance.LogAsync(LogLevel.Info, "Essa é uma mensagem de informação.");
+        await Logger.Instance.LogAsync(LogLevel.Warning, "Essa é uma mensagem de aviso.");
+        await Logger.Instance.LogAsync(LogLevel.Error, "Essa é uma mensagem de erro.");
+    }
+}
